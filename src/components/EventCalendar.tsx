2737c72ddcae8e731a5a5030ee49db9ac30e36cb
PribludaVolka <stepanpan8@gmail.com>
2025-04-27 17:57:53 +0300

    1:32+sc

--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -1,12 +1,11 @@
 "use client"
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Calendar from "react-calendar";
 import 'react-calendar/dist/Calendar.css';
 import Image from "next/image";
 
 type ValuePiece = Date | null;
-
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 const events = [
@@ -31,28 +30,33 @@ const events = [
 ];
 
 const EventCalendar = () => {
-    const [value, onChange] = useState<Value>(new Date());
+    const [value, setValue] = useState<Value>(null);
+
+    useEffect(() => {
+        setValue(new Date());
+    }, []);
+
     return (
-        <div className = "bg-white p-4 rounded-md">
-            <Calendar onChange={onChange} value={value} />
-            <div className = "flex items-center justify-between ">
-                <h1 className = "text-lg font-semibold my-4">Events</h1>
-                <Image src = "/moreDark.png" alt = "" width = {20} height = {20}/>
+        <div className="bg-white p-4 rounded-md">
+            {value && <Calendar onChange={setValue} value={value} />}
+            <div className="flex items-center justify-between">
+                <h1 className="text-lg font-semibold my-4">Events</h1>
+                <Image src="/moreDark.png" alt="" width={20} height={20} />
             </div>
-            <div className = "flex flex-col gap-4">
+            <div className="flex flex-col gap-4">
                 {events.map(event => (
-                    <div className = "p-5 rounded-md border-2 border-gray-100 border-t-4 odd:border-t-schoolSky even:border-t-schoolPurple" 
-                    key = {event.id}>
-                         <div className = "flex items-center justify-between">
-                            <h1 className = "font-semibold text-gray-600">{event.title}</h1>
-                            <span className = "text-gray-300 text-xs">{event.time}</span>
-                         </div>
-                         <p className = "mt-2 text-gray-400 text-sm">{event.descriprion}</p>
+                    <div className="p-5 rounded-md border-2 border-gray-100 border-t-4 odd:border-t-schoolSky even:border-t-schoolPurple"
+                        key={event.id}>
+                        <div className="flex items-center justify-between">
+                            <h1 className="font-semibold text-gray-600">{event.title}</h1>
+                            <span className="text-gray-300 text-xs">{event.time}</span>
+                        </div>
+                        <p className="mt-2 text-gray-400 text-sm">{event.descriprion}</p>
                     </div>
                 ))}
             </div>
         </div>
-    )
-} 
+    );
+}
 
-export default EventCalendar
+export default EventCalendar;
