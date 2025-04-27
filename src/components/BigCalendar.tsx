8e311eb1aca75b97aa7410d9645374fd6640a69e
bim121 <vitisik54@gmail.com>
2025-04-27 18:05:50 +0300

    fix calendar event

--- src/components/BigCalendar.tsx
+++ src/components/BigCalendar.tsx
@@ -22,8 +22,8 @@ const BigCalendar = () => {
             view={view}
             style={{ height: "98%" }}
             onView={handleOnChangeView}
-            min={new Date(2025,1,0,8,0,0)}
-            max={new Date(2025,1,0,19,0,0)}
+            min={new Date(2025, 3, 28, 8, 0, 0)}
+            max={new Date(2025, 3, 28, 19, 0, 0)}
          />
     ); 
 };
