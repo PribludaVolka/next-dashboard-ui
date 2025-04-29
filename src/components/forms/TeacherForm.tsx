7bc51e79bd29d25902097b4b8d51472778548f74
PribludaVolka <stepanpan8@gmail.com>
2025-04-29 21:38:30 +0300

    3:39

--- src/components/forms/TeacherForm.tsx
+++ src/components/forms/TeacherForm.tsx
@@ -3,6 +3,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import InputField from "../InputField";
 
 const schema = z.object({
     username: z
@@ -21,12 +22,15 @@ const schema = z.object({
     lastName: z
         .string()
         .min(1, { message: 'Last name is required!' }),
-    phoneNumber: z
+    phone: z
         .string()
         .min(1, {message: 'Phone number is required!'}),
     address: z
         .string()
-        .min(1, {message: 'Phone number is required!'}),
+        .min(1, {message: 'Address is required!'}),
+    bloodType: z
+        .string()
+        .min(1, {message: 'Blood Type is required!'}),
     birthday: z
         .string()
         .min(1, {message: 'Phone number is required!'}),
@@ -35,6 +39,8 @@ const schema = z.object({
     img: z
     .instanceof(File, {message: 'Image is required!'})
   });
+
+type Inputs = z.infer<typeof schema>; 
 
 const TeacherForm = ({
     type, data
@@ -46,7 +52,7 @@ const TeacherForm = ({
         register,
         handleSubmit,
         formState: { errors },
-      } = useForm({
+      } = useForm <Inputs>({
         resolver: zodResolver(schema),
       });
 
@@ -58,12 +64,15 @@ const TeacherForm = ({
         <form className = "flex flex-col gap-8" onSubmit={onSubmit}>
             <h1 className = "text-xl font-semibold">Create a new teacher</h1>
             <span className = "text-xs text-gray-400 font-medium">Authentication Information</span>
-            <div className = "flex flex-col gap-2 w-full md:w-1/4">
-                <label className = "text-xs text-gray-500">Username</label>
-                <input type="text" {...register("username")} className = "ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm w-full" />
-                {errors.username?.message && <p className = "text-xs text-red-400">{errors.username?.message.toString()}</p>}
-            </div>
+            <InputField label = "Username" name = "username" defaultValue={data?.username} register={register} error={errors?.username}/>
+            <InputField label = "Email" name = "email" defaultValue={data?.email} register={register} error={errors?.email}/>
+            <InputField label = "Password" name = "password" type = "" defaultValue={data?.password} register={register} error={errors?.password}/>
             <span className = "text-xs text-gray-400 font-medium">Personal Information</span>
+            <InputField label = "Last Name" name = "lastName" defaultValue={data?.lastName} register={register} error={errors.lastName}/>
+            <InputField label = "Phone" name = "phone" defaultValue={data?.phone} register={register} error={errors.phone}/>
+            <InputField label = "Address" name = "address" defaultValue={data?.address} register={register} error={errors.address}/>
+            <InputField label = "Blood Type" name = "bloodType" defaultValue={data?.bloodType} register={register} error={errors.bloodType}/>
+            <InputField label = "Birthday" name = "birthday" defaultValue={data?.birthday} register={register} error={errors.birthday} type="date" />
             <button className = "bg-blue-400 text-white p-2 rounded-md">{type === "create" ? "Create" : "Update"}</button>
         </form>
     );
